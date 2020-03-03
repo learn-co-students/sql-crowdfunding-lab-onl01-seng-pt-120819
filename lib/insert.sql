40678c63e316d9d769306c4dc8343bfc58db4050
S. Makai Takori <makait87@gmail.com>
2020-03-03 00:34:26 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,83 @@
+INSERT INTO users (name, age) VALUES
+("Herbert", 23),
+("Makai", 32),
+("Lori", 40),
+("Alfredo", 26),
+("Alona", 30),
+("Gerald", 22),
+("Perry", 33),
+("Krystle", 31),
+("Loris", 27),
+("Dana", 48),
+("Tawana", 43),
+("Catherine", 13),
+("Lucy", 18),
+("Jack", 24),
+("Love", 21),
+("Luna", 36),
+("Todd", 38),
+("Bob", 35),
+("Alex", 28),
+("Maxine", 25);
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES
+("Project A", "Hunger", 10000, "April 1st", "June 1st"),
+("Project B", "Homeless", 20000, "April 20th", "June 20th"),
+("Project C", "Family", 15000, "April 5th", "June 5th"),
+("Project D", "Music Program", 50000, "April 1st", "June 1st"),
+("Project E", "School Books", 20000, "April 1st", "June 1st"),
+("Project F", "Pencils", 4000, "April 1st", "June 1st"),
+("Project G", "Notebooks", 5000, "April 1st", "June 1st"),
+("Project H", "Tutors", 30000, "April 1st", "June 1st"),
+("Project I", "Art Program", 45000, "April 1st", "June 1st"),
+("Project J", "Soup Kitchen", 30000, "April 1st", "June 1st");
+
+INSERT INTO pledges (amount) VALUES
+(20000),
+(10000),
+(1000),
+(5000),
+(5000),
+(15000),
+(20000),
+(500),
+(800),
+(400),
+(6000),
+(900),
+(2500),
+(50),
+(200),
+(2000),
+(100),
+(100),
+(100),
+(300),
+(350),
+(500),
+(20),
+(4500),
+(5200),
+(800),
+(750),
+(600),
+(150000),
+(8500);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
